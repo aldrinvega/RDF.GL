7aa9d8d8d8d3b65c0e7fe8b7200f0c8032749a66
Aldrin Vega <aldrinmundovega@gmail.com>
2024-08-27 10:44:55 +0800

    add: reset password and chane password

---
+++ RDF.GL/Features/Users/ChangePassword.cs
@@ -0,0 +1,63 @@
+﻿using System.Security.Claims;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using RDF.Arcana.API.Features.Users;
+using RDF.GL.Common;
+using RDF.GL.Data;
+
+namespace RDF.GL.Features.Users;
+
+[Route("api/user")]
+[ApiController]
+
+public class ChangePassword(IMediator _mediator) : ControllerBase
+{
+    [HttpPatch("change-password")]
+    public async Task<IActionResult> Patch([FromBody] ChangePasswordCommand request)
+    {
+        try
+        {
+            if (User.Identity is ClaimsIdentity identity
+                && int.TryParse(identity.FindFirst("id")?.Value, out var userId))
+            {
+                request.Id = userId;
+            }
+            var result = await _mediator.Send(request);
+            return result.IsSuccess ? Ok(result) : BadRequest(result);
+        }catch(Exception ex)
+        {
+            return BadRequest(ex.Message);
+        }
+    }
+    public class ChangePasswordCommand : IRequest<Result>
+    {
+        public int Id { get; set; }
+        public string OldPassword { get; set; }
+        public string NewPassword { get; set; }
+    }
+
+    public class Handler(ProjectGLDbContext _context) : IRequestHandler<ChangePasswordCommand, Result>
+    {
+        public async Task<Result> Handle(ChangePasswordCommand request, CancellationToken cancellationToken)
+        {
+            var users = await _context.Users
+                .FirstOrDefaultAsync(u => u.Id == request.Id, cancellationToken);
+
+            if (users == null)
+            {
+                return UserErrors.NotFound();
+            }
+
+            if (!BCrypt.Net.BCrypt.Verify(request.OldPassword, users.Password))
+            {
+                return UserErrors.OldPasswordIncorrect();
+            }
+
+            users.Password = BCrypt.Net.BCrypt.HashPassword(request.NewPassword);
+
+            await _context.SaveChangesAsync(cancellationToken);
+            return Result.Success();
+        }
+    }
+}
---
+++ RDF.GL/Features/Users/ResetPassword.cs
@@ -0,0 +1,55 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using RDF.Arcana.API.Features.Users;
+using RDF.GL.Common;
+using RDF.GL.Data;
+
+namespace RDF.GL.Features.Users;
+
+[Route("api/user")]
+[ApiController]
+
+public class ResetPassword(IMediator _mediator) : ControllerBase
+{
+    [HttpPatch("{id:int}/reset-password")]
+    public async Task<IActionResult> Patch([FromRoute] int id)
+    {
+        try
+        {
+            var command = new ResetPasswordCommand
+            {
+                UserId = id
+            };
+            var result = await _mediator.Send(command);
+            return result.IsSuccess ? Ok(result) : BadRequest(result);
+        }catch(Exception ex)
+        {
+            return BadRequest(ex.Message);
+        }
+        
+    }
+    public class ResetPasswordCommand : IRequest<Result>
+    {
+        public int UserId { get; set; }
+    }
+
+    public class Handler(ProjectGLDbContext _context) : IRequestHandler<ResetPasswordCommand, Result>
+    {
+        public async Task<Result> Handle(ResetPasswordCommand request, CancellationToken cancellationToken)
+        {
+            var user = await _context.Users
+                .FirstOrDefaultAsync(u => u.Id == request.UserId, cancellationToken);
+
+            if (user == null)
+            {
+                return UserErrors.NotFound();
+            }
+
+            user.Password = BCrypt.Net.BCrypt.HashPassword(user.Username);
+
+            await _context.SaveChangesAsync(cancellationToken);
+            return Result.Success();
+        }
+    }
+}
